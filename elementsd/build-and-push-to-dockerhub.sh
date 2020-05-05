f07d86ad0788b6b6b5b59b1ac2e9b09f695004af
Restyled.io <commits@restyled.io>
2020-05-05 15:38:38 +0000

    Restyled by shellharden

--- elementsd/build-and-push-to-dockerhub.sh
+++ elementsd/build-and-push-to-dockerhub.sh
@@ -4,12 +4,12 @@ export VER=0.18.1.7
 
 docker pull blockstream/elementsd:latest
 #docker build --cache-from blockstream/elementsd:latest -t blockstream/elementsd:${VER} . -f Dockerfile.gitian || { echo -e "\nSomething broke"; exit 1; }
-docker build --cache-from blockstream/elementsd:latest -t blockstream/elementsd:${VER} . || { echo -e "\nSomething broke"; exit 1; }
-docker push blockstream/elementsd:${VER}
+docker build --cache-from blockstream/elementsd:latest -t blockstream/elementsd:"$VER" . || { echo -e "\nSomething broke"; exit 1; }
+docker push blockstream/elementsd:"$VER"
 ## Uncomment to push :latest tag
-docker tag blockstream/elementsd:${VER} blockstream/elementsd:latest
+docker tag blockstream/elementsd:"$VER" blockstream/elementsd:latest
 docker push blockstream/elementsd:latest
 
-SHA=$(docker inspect --format='{{index .RepoDigests 0}}' blockstream/elementsd:${VER})
+SHA=$(docker inspect --format='{{index .RepoDigests 0}}' blockstream/elementsd:"$VER")
 
-echo -e "The new image is:\n${SHA}"
+echo -e "The new image is:\n$SHA"
--- lightningd/build-and-push-to-dockerhub.sh
+++ lightningd/build-and-push-to-dockerhub.sh
@@ -3,10 +3,10 @@
 export VER=v0.8.2
 
 docker pull blockstream/lightningd:latest
-docker build --cache-from blockstream/lightningd:latest -t blockstream/lightningd:${VER} -t blockstream/lightningd:latest . || { echo -e "\nSomething broke"; exit 1; }
+docker build --cache-from blockstream/lightningd:latest -t blockstream/lightningd:"$VER" -t blockstream/lightningd:latest . || { echo -e "\nSomething broke"; exit 1; }
 docker push blockstream/lightningd:latest
-docker push blockstream/lightningd:${VER}
+docker push blockstream/lightningd:"$VER"
 
 SHA=$(docker inspect --format='{{index .RepoDigests 0}}' blockstream/lightningd:latest)
 
-echo -e "The new image is:\n${SHA}"
+echo -e "The new image is:\n$SHA"
